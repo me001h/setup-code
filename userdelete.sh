e595bac7ce7c28e062cd536a14ea727f4a44142b
ehomecore91 <32506079+ehomecore91@users.noreply.github.com>
2017-10-04 14:30:48 +0800

    Create userdelete.sh

---
+++ userdelete.sh
@@ -0,0 +1,22 @@
+user-list.sh
+#!/bin/bash
+
+echo""
+
+read -p "Masukkan Username yang ingin anda hapus: " username
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+# proses mengganti passwordnya
+userdel -f $username
+
+  echo "Script by overses.net"
+  echo "Terimakasih sudah berlangganan di @orangkuatsabahanterkini"
+  echo " "
+  echo "-----------------------------------------------"
+  echo -e "Username ${blue}$username${NC} Sudah berhasil di ${red}HAPUS${NC}."
+  echo -e "Akses Login untuk username ${blue}$username${NC} sudah dihapus"
+  echo "-----------------------------------------------"
+else
+echo "Username ${red}$username${NC} tidak ditemukan di VPS anda"
+    exit 1
+    fi
